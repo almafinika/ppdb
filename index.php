d65d2815f9760a9a346b2381dd8519ade1d88456
mdradityatama <mdimasradityatama@gmail.com>
2020-05-30 16:39:15 +0700

    ngerapihin struktur folder, dan rapihin index

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PPDB</title>
+    
+    <!-- menyisipkan bootstrap -->
+     <link rel="stylesheet" href="css/bootstrap.min.css" />
+</head>
+<body class="bg-light">
+    <header>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-9">
+                        <!--style logo header-->
+                        <h1 style="text-align: center"> WELCOME TO PPDB ONLINE </h1>         
+                        <div class="row">
+                            <div class="col-3">               
+                                <div style="width: 100%"; height: 150px;">
+                                    <img src= "img/logosmanding.png" style= width:150px; height:150px; />
+                                </div>
+                            </div>
+                            <div class="col">
+                                <p align="justify">Selamat datang di website PPDB Online SMAN 1 Gadingrejo yang merupakan wujud dari kesungguhan kami untuk membangun media komunikasi interaktif demi meningkatkan kualitas pendidikan di Sekolah ini. Kami menyadari website ini masih banyak kekurangan yang perlu dibenahi, untuk itu kami berharap masukan dan saran dari pengunjung semua demi sempurnanya website ini. Terimakasih, dan semoga bermanfaat bagi kita semua.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col ml-auto">
+                        <a href="form-daftar.php" class="btn btn-secondary">Daftar</a>
+                        <a href="login.php" class="btn btn-success">Masuk</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-50">
+                    <img class="img img-responsive" src="img/gambar.jpg" />
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PPDB</title> -->
+
+    <!-- menyisipkan bootstrap -->
+     <!-- <link rel="stylesheet" href="css/bootstrap.min.css" />
+</head>
+
+<body class="bg-light">
+    <header>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col"> -->
+                        <!--style logo header-->
+                        <!-- <h1 align="left"> WELCOME TO PPDB ONLINE </h1>
+                        <div style="width: 150px"; height: 150px; float: left">
+                            <img src= "img/logosmanding.png" style= width:150px; height:150px; />
+                        </div>
+                    </div>
+                <div class="col">
+                    <div class="col-md-4">
+                        <a href="form-daftar.php" class="btn btn-secondary">Daftar</a>
+                        <a href="login.php" class="btn btn-success">Masuk</a>
+                    </div></div>
+                </div>
+                <div class="row">
+                    <div class="col-md-8"> -->
+                        <!--style logo header-->
+                        <!-- <h1 align="left"> WELCOME TO PPDB ONLINE </h1>
+                        <div style="width: 150px"; height: 150px; float: left">
+                            <img src= "img/logosmanding.png" style= width:150px; height:150px; />
+                        </div> -->
+                        <!--style text header-->
+                        <!-- <div stye="width: 100px; height:10px; margin=justify: 100px">
+                        <div style="text=align:justify;font-size:15px;font-family: MS Serif", "New York", sans-serif;">
+                        </br>
+                        <p align="justify">Selamat datang di website PPDB Online SMAN 1 Gadingrejo yang merupakan wujud dari kesungguhan kami untuk membangun media komunikasi interaktif demi meningkatkan kualitas pendidikan di Sekolah ini. Kami menyadari website ini masih banyak kekurangan yang perlu dibenahi, untuk itu kami berharap masukan dan saran dari pengunjung semua demi sempurnanya website ini. Terimakasih, dan semoga bermanfaat bagi kita semua.</p>
+                        </div>
+                    </div>
+                    </div>
+                    <div class="col-md-4">
+                        <a href="form-daftar.php" class="btn btn-secondary">Daftar</a>
+                        <a href="login.php" class="btn btn-success">Masuk</a>
+                    </div>
+
+            </div>
+        </div>
+    </header>
+
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-50">
+                    <img class="img img-responsive" src="img/gambar.jpg" />
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body> -->
